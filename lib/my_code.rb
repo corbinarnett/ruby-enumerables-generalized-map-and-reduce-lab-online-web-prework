d4d98a07f6681778252594214aac81bac49fd580
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-15 02:48:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,15 +11,15 @@ end
 
 def reduce(source_array, starting_point=nil)
   if starting_point
-    accum = starting_point
+    total = starting_point
     i = 0
   else
-    accum = source_array[0]
+    total = source_array[0]
     i = 1
   end
   while i < source_array.length
-    accum = yield(accum, source_array[i])
+    total = yield(total, source_array[i])
     i += 1
   end
-  accum
+  total
 end
